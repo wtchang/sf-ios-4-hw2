01065cfd7a6ef2c16d4b425162935adbcfc8756b
Kathryn Rotondo <krotondo@gmail.com>
2015-12-09 18:05:35 -0800

    added playground

---
+++ iOS4-Assessment-2.playground/Contents.swift
@@ -0,0 +1,66 @@
+// iOS4 Assessment 2
+
+// Complete the following @todos. Write your code
+// immediately below the relevant instructions.
+
+// 1. Write a variable to hold the name of a dog and
+//    set its value to a dog's name
+
+// 2. Write a variable to hold the duration of a movie
+//    in minutes and set its value
+
+// 3. Write a variable to hold whether it is daytime
+//    and set it to reflect that it is nighttime
+
+// 4. Write a constant to store PI (3.14159)
+
+// 5. Write a constant to store your favorite holiday
+
+// 6. Print out a message to the console saying 
+//    "My favorite holiday is ___" 
+//    (fill in the blank using the variable from #5)
+
+// 7. Store your first and last names in constants.
+//    Use String concatenation to store your full
+//    name in a constant called name
+
+// 8. Use String interpolation to write a message
+//    greeting yourself, i.e. "Hello ____ _____"
+//    using the name variable from #7
+
+// 9. Write a multiline comment explaining the
+//    difference between ints, floats, and doubles
+
+// 10. Write a single line comment reminding yourself
+//    of the action item to submit this homework.
+
+// 11. Declare an optional String called religion
+//     and do not provide a value
+
+// 12. Declare an optional variable for an hourly rate
+//     and set it equal to a dollar and cents amount
+
+// 13. Print out the hourly rate to the console using
+//     String interpolation, making sure it does not 
+//     show "Optional(__)" in the output.
+
+// 14. Using the variable from #3, write a conditional
+//     that prints "Hello!" to the console if it is day
+//     and "Goodnight!" to the console if it is night
+
+// 15. Using the variable from #2, write a conditional
+//     that says "too short" if it is less than an hour,
+//     "too long" if it is more than two hours, and
+//     "just right" if it is between the two
+
+// 16. Create two constants a and b and set them equal 
+//     to 5 and 7, respectively.
+
+// 17. Create a variable sum that computes the sum from #16
+
+// 18. Create a variable difference that computes a minus b.
+
+// 19. Create a variable product that computes a times b.
+
+// 20. Create a variable quotient that computes a divided
+//     by b.
